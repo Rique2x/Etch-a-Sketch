bb1c92a0fbb3c36d79735e61ca5c0d29a532216e
Rique2 <enrique2heinrich@gmail.com>
2023-01-07 11:41:36 +0200

    Targeting the nodes with Selectors(script.js)

---
+++ script.js
@@ -0,0 +1,6 @@
+//Targeting the nodes with Selectors
+
+const container = document.querySelector ('.container');
+const pickUpColor = document.querySelector ('.base');
+const colorButtons = document.querySelector ('.btn');
+let color = 'black';
